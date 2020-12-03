ad9ce10748d0a41c4469aa130a094f17773ea851
Mikhail Smirnov <smirnov@alfasatcom.ru>
2020-12-03 16:48:04 +0300

    почистил немного в graph-plot

--- src/components/main-panel/algo-orders-panel/graph-modal/plot-chart.js
+++ src/components/main-panel/algo-orders-panel/graph-modal/plot-chart.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
-// import SelectRefresh from './select-refresh/select-refresh';
 
 const PlotChart=(props)=>{
 	
 		function unpack(rows, key) {
-		  // debugger;
 		  return rows.map(function(row) { return row[key]; });
 		}
 		var	data=[
@@ -18,12 +16,10 @@ const PlotChart=(props)=>{
 			];
 
 	    var layout = {
-		  showlegend:true,
-		  showtitle:false,
-		  // title: props.city,
 		  xaxis: {
-			autorange: true,
-		    rangeslider: {range: ['2020-01-11', '2020-31-12'],
+			autorange:true,
+			// autorange: true,
+		    rangeslider:{range: ['2020-01-11', '2020-31-12'],
 		  },
 		  yaxis: {
 			autorange:true,
@@ -31,22 +27,8 @@ const PlotChart=(props)=>{
 		  }
 		}
 		};
-	// const changeTimeRefresh=(e)=>{
-	// 	const time=e.target.value;
-	// 	props.setTimeRefresh(time);
-	// };
-	// const options=props.timeRefreshArray.map(el=>(
-	// 		<option>
-	// 			{el}
-	// 	    </option>));
 	return (
 		<div className='card'>
-		 {/*<div>*/}
-			{/*<SelectRefresh*/}
-				{/*options={options}*/}
-				{/*changeTimeRefresh={changeTimeRefresh}*/}
-			{/*/>*/}
-		 {/*</div>*/}
 		  <Plot
 			className='pr-3'
 			data={data}
